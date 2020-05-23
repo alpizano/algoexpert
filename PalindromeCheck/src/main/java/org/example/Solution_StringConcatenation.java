85e0aad81abcb193ad289970c27e679d5d98b4e6
alpizano <apizano@purdue.edu>
2020-05-23 18:43:58 -0500

    Refactored to include static method isPalindrome

--- PalindromeCheck/src/main/java/org/example/Solution_StringConcatenation.java
+++ PalindromeCheck/src/main/java/org/example/Solution_StringConcatenation.java
@@ -6,16 +6,23 @@ package org.example;
  */
 public class Solution_StringConcatenation
 {
+    public static boolean isPalindrome(String str) {
+        // Write your code here.
+        String reverseString = "";
 
-    public static void main( String[] args )
-    {
-        String test = "Palindrome";
-        String reverse = "";
-
-        for(int i =0; i<test.length(); i++) {
-            reverse = test.charAt(i) + reverse;
+        // Loop through String and concatenate reverse in-place
+        for(int i =0; i<str.length(); i++) {
+            reverseString = str.charAt(i) + reverseString;
         }
 
-        System.out.println(reverse);
+        return reverseString.equals(str);
+    }
+    public static void main( String[] args )
+    {
+        String arg1 = "Palindrome";
+        String arg2 = "rar";
+
+        System.out.println(Solution_StringConcatenation.isPalindrome(arg1));
+        System.out.println(Solution_StringConcatenation.isPalindrome(arg2));
     }
 }
