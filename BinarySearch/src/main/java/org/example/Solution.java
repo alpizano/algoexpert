05c38de375144ac1fe5218a2f6b91b832fed18a3
alpizano <apizano@purdue.edu>
2020-05-25 11:39:40 -0500

    Adding BinarySearch to project

---
+++ BinarySearch/src/main/java/org/example/Solution.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Solution
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
