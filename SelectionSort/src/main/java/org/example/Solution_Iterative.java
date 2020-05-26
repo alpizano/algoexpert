aad8dd07ced8499058508e25ba9a096248f13a89
alpizano <apizano@purdue.edu>
2020-05-25 20:14:42 -0500

    Implementing SelectionSort algorithm

--- SelectionSort/src/main/java/org/example/Solution_Iterative.java
+++ SelectionSort/src/main/java/org/example/Solution_Iterative.java
@@ -1,10 +1,29 @@
 package org.example;
 
 
-public class Solution_Iterative
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Solution_Iterative {
+    public static void main(String[] args) {
+        int[] array = new int[]{8, 5, 2, 9, 5, 6, 3};
+
+        boolean found = false;
+
+        for (int i = 0; i < array.length; i++) {
+            int lowestIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[lowestIndex]) {
+                    lowestIndex = j;
+                    found = true;
+                }
+                if (j == array.length - 1 && found == true) {
+                    int tmp = array[i];
+                    array[i] = array[lowestIndex];
+                    array[lowestIndex] = tmp;
+                    found = false;
+                }
+            }
+        }
+        for (int i : array) {
+            System.out.printf("%d ", i);
+        }
     }
 }
