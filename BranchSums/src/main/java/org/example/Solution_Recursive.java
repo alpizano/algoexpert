f1cd0f3c60013b4a9699212d934cae35e1f85410
alpizano <apizano@purdue.edu>
2020-05-26 01:43:13 -0500

    Added Recursive Solution for Binary Tree

--- BranchSums/src/main/java/org/example/Solution_Recursive.java
+++ BranchSums/src/main/java/org/example/Solution_Recursive.java
@@ -1,9 +1,10 @@
 package org.example;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+public class Solution_Recursive {
     public static class BinaryTree {
         int value;
         BinaryTree left;
@@ -17,11 +18,32 @@ public class Solution {
     }
 
     public static List<Integer> branchSums(BinaryTree root) {
-        return null;
+        List<Integer> sums = new ArrayList<>();
+
+        return calculateBranchSums(root, 0, sums);
+
+    }
+
+    public static List<Integer> calculateBranchSums(BinaryTree node, int runningSum, List sums) {
+        if(node == null) {
+            return sums;
+        }
+        int newRunningSum = runningSum + node.value;
+
+        if(node.left == null && node.right == null) {
+            sums.add(newRunningSum);
+            return sums;
+        }
+
+        calculateBranchSums(node.left, newRunningSum, sums) ;
+        calculateBranchSums(node.right, newRunningSum, sums);
+        return sums;
+
     }
 
     public static void main( String[] args )
     {
+        // Constructing sample Binary Tree input
         BinaryTree tree = new BinaryTree(1);
         tree.left = new BinaryTree(2);
         tree.left.left = new BinaryTree(4);
@@ -33,8 +55,8 @@ public class Solution {
         tree.right.left = new BinaryTree(6);
         tree.right.right = new BinaryTree(7);
 
-        for(Integer i: Solution.branchSums(tree)) {
-            System.out.printf("%d" , i);
+        for(Integer i: Solution_Recursive.branchSums(tree)) {
+            System.out.printf("%d " , i);
         }
     }
 }
