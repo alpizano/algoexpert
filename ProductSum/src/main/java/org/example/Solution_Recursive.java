aff8acf97419f10d8ae8a5e1be2e74a644acd555
alpizano <apizano@purdue.edu>
2020-05-27 00:00:01 -0500

    Implementing algo

--- ProductSum/src/main/java/org/example/Solution_Recursive.java
+++ ProductSum/src/main/java/org/example/Solution_Recursive.java
@@ -5,15 +5,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution_Recursive {
+    static int sum = 0;
+    static int factor =1;
+
     public static int productSum(List<Object> array) {
-        return 0;
+        sum =0;
+        factor =1;
+
+        for(Object o: array) {
+            if(o instanceof Integer) {
+                sum = sum + factor*(int)o;
+            }
+            else {
+                // Found special array. Recursive function
+                productSum(array);
+            }
+        }
+
+        return sum;
+    }
+
+    public void helper(List<Object> array) {
+
     }
 
     public static void main(String[] args) {
         // Instantiating Lists
-        List<Object> list = new ArrayList<Object>(Arrays.asList(5, 2, Arrays.asList(7,-1), 3, Arrays.asList(6, Arrays.asList(-13,8), 4)));
+        List<Object> list = new ArrayList(Arrays.asList(5, 2, new ArrayList(Arrays.asList(7,-1)), 3, new ArrayList(Arrays.asList(6, new ArrayList(Arrays.asList(-13,8)), 4))));
 
         System.out.println(list);
-        System.out.println(list.get(1) instanceof ArrayList);
+        System.out.println(list.get(0) instanceof Integer);
+        System.out.println(list.get(4) instanceof ArrayList);
+
+        List<Object> sample = (List) list.get(4);
+        System.out.println(sample.get(1));
+
+        System.out.println(((List)list.get(4)).get(1));
+
+        System.out.println(Solution_Recursive.productSum(list));
     }
 }
