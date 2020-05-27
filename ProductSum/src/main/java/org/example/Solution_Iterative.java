336bbdf3eb093f2a114c786f8750cf5b57698232
alpizano <apizano@purdue.edu>
2020-05-26 23:43:17 -0500

    Condensing down sample input instantiation of nested Lists

--- ProductSum/src/main/java/org/example/Solution_Iterative.java
+++ ProductSum/src/main/java/org/example/Solution_Iterative.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution_Iterative
@@ -9,23 +10,7 @@ public class Solution_Iterative
     public static void main( String[] args )
     {
         // Create sample list
-        List<Object> list = new ArrayList<>();
-        List<Object> list2 = new ArrayList<>();
-        List<Object> list3 = new ArrayList<>();
-        List<Object> list4 = new ArrayList<>();
-
-        list.add(5);
-        list.add(2);
-        list2.add(7);
-        list2.add(-1);
-        list.add(list2);
-        list.add(3);
-        list3.add(6);
-        list4.add(-13);
-        list4.add(8);
-        list3.add(list4);
-        list.add(list3);
-        list3.add(4);
+        List<Object> list = new ArrayList<Object>(Arrays.asList(5, 2, Arrays.asList(7,-1), 3, Arrays.asList(6, Arrays.asList(-13,8), 4)));
 
         System.out.println(list);
         System.out.println(list.get(1) instanceof ArrayList);
