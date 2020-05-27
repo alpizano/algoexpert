31740fbc82c928fd9c141e95be668cf51cf51d6c
alpizano <apizano@purdue.edu>
2020-05-26 23:22:48 -0500

    Beginning algorithm for product sum

--- ProductSum/src/main/java/org/example/Solution.java
+++ ProductSum/src/main/java/org/example/Solution.java
@@ -1,10 +1,26 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Object> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        List<Object> list2 = new ArrayList<>();
+        list2.add(7);
+        list2.add(-1);
+        list.add(list2);
+
+        System.out.println(list);
+        System.out.println(list.get(1) instanceof ArrayList);
+
+        for(int i=0; i<array.length; i++) {
+
+        }
     }
 }
