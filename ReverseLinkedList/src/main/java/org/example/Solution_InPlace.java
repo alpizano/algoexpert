ebee758444448ebf3a7aef5b0e88f0ed51284dbf
alpizano <apizano@purdue.edu>
2020-06-12 03:20:37 -0500

    Handling edge case where input is 1 node

--- ReverseLinkedList/src/main/java/org/example/Solution_InPlace.java
+++ ReverseLinkedList/src/main/java/org/example/Solution_InPlace.java
@@ -3,6 +3,11 @@ package org.example;
 public class Solution_InPlace {
     public static LinkedList reverseLinkedList(LinkedList head) {
 
+        // Handling edge case where input is just 1 node
+        if(head.next == null) {
+            return head;
+        }
+
         LinkedList p1 = head; // points to 0
         LinkedList p2 = p1.next; // points to 1
         LinkedList tmp = p2.next; // points to 2
@@ -12,6 +17,7 @@ public class Solution_InPlace {
            // Point node in reverse
             p2.next = p1;
 
+            // Null out first nodes next reference or infinite cyclic loop
            if(p1 == head) {
                head.next =null;
            }
@@ -19,6 +25,8 @@ public class Solution_InPlace {
            // Shift nodes
            p1 = p2;
            p2 = tmp;
+
+           // If tmp is already at null, make sure not to increment it anymore
            if(tmp != null) {
                tmp = tmp.next;
            }
