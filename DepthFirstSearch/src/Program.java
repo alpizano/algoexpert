87270e9710152b92d029703ad7147821d9050083
kbrv37 <Al.pizano@motorolasolutions.com>
2020-10-21 02:27:04 -0500

    adding base class for q

---
+++ DepthFirstSearch/src/Program.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Program {
+    // Do not edit the class below except
+    // for the depthFirstSearch method.
+    // Feel free to add new properties
+    // and methods to the class.
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            // Write your code here.
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
