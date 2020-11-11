bfe5702160fcfd27bfbf8c3b5176e6f53b8f030c
kbrv37 <Al.pizano@motorolasolutions.com>
2020-11-10 20:52:07 -0600

    starting BST construction problem

---
+++ src/binarytrees/BSTConstruction.java
@@ -0,0 +1,34 @@
+package binarytrees;
+
+public class BSTConstruction {
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+
+        public BST insert(int value) {
+            // Write your code here.
+            // Do not edit the return statement of this method.
+            return this;
+        }
+
+        public boolean contains(int value) {
+            // Write your code here.
+            return false;
+        }
+
+        public BST remove(int value) {
+            // Write your code here.
+            // Do not edit the return statement of this method.
+            return this;
+        }
+    }
+    public static void main(String[] args) {
+        BSTConstruction bst = new BSTConstruction();
+
+    }
+}
